4198441b637651067379e410d0a4f45d05cc59a0
Nishant Minerva <nishantcafe@gmail.com>
2024-08-08 12:14:30 +0530

    Initial Commit

---
+++ BlinkIt-Assignmnet/BlinkIt_AssignmnetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkIt_AssignmnetApp.swift
+//  BlinkIt-Assignmnet
+//
+//  Created by Nishant Minerva on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct BlinkIt_AssignmnetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
